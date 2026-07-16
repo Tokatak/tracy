4b3ca6a92c144ebda63e438cc5326b787e4b7131
Egor Bashinsky <nelzeabeztak@gmail.com>
2026-07-16 21:10:20 +0300

    migrate from bat to makefile

--- build.bat
+++
@@ -1,5 +0,0 @@
-:: debug
-gcc -Wall -Wextra -Wpedantic -g -O0 -o main main.c include/ray.c include/tracer.c include/ppm.c && main.exe
-
-:: optimized
-:: gcc -Wall -Wextra -Wpedantic -O3 -march=native -o main main.c include/ray.c include/tracer.c include/ppm.c && main.exe
